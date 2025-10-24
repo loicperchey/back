3a6d49f44f54668d11736d06d750b6dd4a8dc147
dagonlp <loic.perchey@free.fr>
2025-10-24 07:26:59 +0200

    mise à jour dépendances et ajout premier controller

--- service/fleshAndBloodCardService.js
+++ service/fleshAndBloodCardService.js
@@ -1,5 +1,4 @@
-
-const { default: fastifyMysql } = require("@fastify/mysql");
+const SQL = require('@nearform/sql')
 const fastify = require('fastify')({
   ajv: {
     plugins:[
@@ -10,7 +9,7 @@ const fastify = require('fastify')({
 const ourDbConnector = require("../our-db-connector");
 const Ajv = require("ajv");
 const {postSchemaFleshAndBloodCard} = require("../model/fleshAndBloodCard");
-const { request, json } = require("express");
+const { request, json, response } = require("express");
 const { req } = require("pino-std-serializers");
 const ajv = new Ajv({
    removeAdditional: 'all',
@@ -30,30 +29,65 @@ async function FleshAndBloodCardRoute (fastify, options) {
 
 const schema = postSchemaFleshAndBloodCard;
   
-  
-
-    /* fastify.get('/FleshAndBloodCard/:name', async (request) => {
-     console.log(Object.hasOwn(request.params, 'name'));
-     return{hello: request.params.name};
-    }) */
     
   fastify.get('/', async (request) => {
      return{hello: "bienvenue sur cette piètre tentative de crud sur fastify"};
     })
 
-  fastify.get('/FleshAndBloodCard/:id', (req, reply) => {
-    'SELECT id, name FROM FleshAndBloodCard WHERE id=?', [req.params.id],
-    function onResult (err, result) {
-      reply.send(err || result)
-    }
+  fastify.get('/FleshAndBloodCard/:id', async (request, reply) => {
+    const {id} = request.params;
+    const table="FleshAndBloodCard";
+    const requete = SQL `SELECT * FROM ${table} WHERE id=${id}`;
+    const reponse = fastify.mysql.query(requete);
+    reply.code(200);
+    return {reponse}
 });
 
  // Définition d'une route POST
-fastify.post("/FleshAndBloodCard", schema, async (request, reply) => {
-   
-  
-   reply.code(201).send(request.body);
+fastify.post("/FleshAndBloodCard", async (request, reply) => {
+   const table="FleshAndBloodCard";
+	const { name, edition, year, cost, pitch, type, talent, text, attack, defense } = request.body;
+
+	const requete = SQL`INSERT Into ${table} (name, edition, year, cost, pitch, type, talent, text, attack, defense) VALUES (${name}, ${edition}, ${year}, ${cost}, ${pitch}, ${type}, ${talent}, ${text}, ${attack}, ${defense})`;
+  const reponse = fastify.mysql.query(requete);
+  const {id} = reponse.requete.params.id;
+  const requeteInformationCreation = SQL `SELECT * FROM ${table} WHERE id=${id}`;
+   reply.code(201);
+   return{requeteInformationCreation}
 });
+
+
+
+fastify.put('FleshAndBloodCard/:id', async (request, reply) => {
+	const { id } = request.params;
+	const table = 'FleshAndBloodCard';
+
+	const { name, edition, year, cost, pitch, type, talent, text, attack, defense } = request.body;
+
+	const requete = SQL`UPDATE ${table} SET  name = ${name}, edition = ${edition}, year = ${year}, cost = ${cost}, pitch = ${pitch}, type = ${type}, talent = ${talent}, text = ${text}, attack = ${attack}, defense = ${defense} WHERE id = ${id}`;
+
+	const response = fastify.mysql.query(requete);
+
+	reply.code(200);
+	return { response };
+});
+
+fastify.delete('FleshAndBloodCard/:id', async (request, reply) => {
+	const { id } = request.params;
+	const table = 'fleshAndBloodCard';
+	const requete = SQL`DELETE ${table} WHERE id = ${id}`;
+
+	const response = fastify.mysql.query(requete);
+
+	reply.code(200);
+	return { response };
+});
+
+
+
+
+
 }
+
 
    module.exports = FleshAndBloodCardRoute;
